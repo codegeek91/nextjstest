80bb98eeb2138a165590f18d2ae53a820ae9090a
codegeek1991 <codegeek1991@gmail.com>
2024-01-13 11:54:08 -0500

    updating app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import homeImg from "public/home.jpg";
+import Hero from "@/components/hero";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <Hero
+      imgData={homeImg}
+      imgAlt="car factory"
+      title="Professional Cloud Hosting"
+    />
+  );
 }
